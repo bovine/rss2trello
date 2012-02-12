91bfb21ccf74a51315ef179d148ab9127731d601
Jeff Lawson <jeff@bovine.net>
2012-02-12 01:12:39 -0600

    implement full logic to scan RSS feed for new entries and create Trello cards for them.

--- main.js
+++ main.js
@@ -4,35 +4,103 @@ var Trello = require('./node-trello');
 var config = require('./config');
 
 
-var t = new Trello(config.trello_key, config.trello_token);
+var tr = new Trello(config.trello_key, config.trello_token);
+var targetListId = null;
+var existingCards = null;
+var foundArticles = null;
 
-t.get("/1/boards/"+config.board_id+"/lists/open", function(err, data) {
-    if(err) throw err;
-    console.log(data);
-    data.forEach(function(val) {
-	if (val.name == config.target_list_name) {
-	    console.log(val.id);
-	    
-	    t.post("/1/lists/"+val.id+"/cards", { "name": "moo", "desc": "cow"}, function(err, data) {
-		if(err) throw err;
-		console.log(data);
-	    });
+
+function rememberExistingCard(card) {
+    if (existingCards == null) existingCards = new Array();
+
+    existingCards.push(card);
+}
+
+function checkCardExistsForArticle(article) {
+
+    var found = false;
+    existingCards.forEach(function (card) {
+	// compare card.{name|desc} with article.{title|link|description}
+	// TODO: this might need to be customized based on your RSS feed format.
+	if (card.desc.indexOf(article.link) >= 0) {
+	    found = true;
 	}
-	//console.log(val);
     });
-});
+
+    return found;
+}
+
+function createCardFromArticle(listId, article) {
+    // do some final transformations on the details of the card.
+    // TODO: this might need to be customized based on your RSS feed format.
+    var finalName = article.title.split(',', 1);
+    var finalDesc = article.title + "\n" + article.link + "\n" + article.description;
+
+    // actually create the card on Trello in the correct list.
+    util.puts("Creating card for " + finalName);
+    tr.post("/1/lists/"+listId+"/cards", { "name": finalName, "desc": finalDesc}, function(err, data) {
+	if(err) throw err;
+	//console.log(data);
+	util.puts("Successfully created card id " + data.id);
+    });
+}
 
 
+function processCompletion() {
 
-// var response = rss.parseURL(config.feed_url, function(articles) {
-//     util.puts(articles.length);
-//     for(i=0; i<articles.length; i++) {
-// 	util.puts("Article: "+i+", "+
-// 		 articles[i].title+"\n"+
-// 		 articles[i].link+"\n"+
-// 		 articles[i].description+"\n"+
-// 		 articles[i].content
-// 		);
-//     }
-// });
+    if (targetListId != null && existingCards != null && foundArticles != null) {
+	// Both of the asynchronous requests have finished, so do the actual insertion work.
+
+	util.puts("Found targetListId = " + targetListId);
+	util.puts("Found " + existingCards.length + " existing Trello cards");
+	util.puts("Found " + foundArticles.length + " possible RSS articles");
+
+	foundArticles.forEach(function(article) {
+	    //console.log(article);
+	    
+	    if (!checkCardExistsForArticle(article)) {
+		createCardFromArticle(targetListId, article);
+	    }
+	});
+    }
+}
+
+
+function main() {
+
+    //
+    // Fetch all of the RSS items asynchronously.
+    //
+    rss.parseURL(config.feed_url, function(articles) {
+	// Save the articles and process them when the Trello query is finished.
+	foundArticles = articles;
+	processCompletion();
+    });
+    
+
+    //
+    // Fetch all of the lists and cards in this Trello board asynchronously.
+    //
+    tr.get("/1/boards/"+config.board_id+"/lists/open", function(err, data) {
+	if(err) throw err;
+	data.forEach(function(tlist) {
+	    //console.log(val);
+
+	    // If this is the list we're looking for, remember its id.
+	    if (tlist.name == config.target_list_name) {
+		targetListId = tlist.id;
+	    }
+
+	    // Save the identities of all cards we find, to avoid duplicates.
+	    tlist.cards.forEach(function(card) {
+		rememberExistingCard(card);
+	    });
+	});
+	processCompletion();
+    });
+
+}
+
+
+main();
 
